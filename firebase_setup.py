00f4ffca925df125b93de7fdf7af31a91f5a25d9
Rishika Verma <rv5557@srmist.edu.in>
2025-08-29 23:20:29 +0530

    Update firebase_setup.py

--- firebase_setup.py
+++ firebase_setup.py
@@ -8,7 +8,7 @@ const firebaseConfig = {
   apiKey: "YOUR_API_KEY",   
   authDomain: "your-project.firebaseapp.com", 
   projectId: "your-project-id",         
-  storageBucket: "your-project.appspot.com",
+  storageBucket: "your-project.appspot.com",       
   messagingSenderId: "SENDER_ID",
   appId: "APP_ID"  
 }; 
