c235b11c55b565f18b138c79b6dfe661ec5c9aad
Rishika Verma <rv5557@srmist.edu.in>
2025-08-29 23:20:20 +0530

    Update firebase_setup.py

--- firebase_setup.py
+++ firebase_setup.py
@@ -7,7 +7,7 @@ import { getFirestore } from "firebase/firestore";
 const firebaseConfig = {   
   apiKey: "YOUR_API_KEY",   
   authDomain: "your-project.firebaseapp.com", 
-  projectId: "your-project-id",  
+  projectId: "your-project-id",         
   storageBucket: "your-project.appspot.com",
   messagingSenderId: "SENDER_ID",
   appId: "APP_ID"  
